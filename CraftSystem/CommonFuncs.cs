1abead33dcaea1fa4c20c568e6db51c8b55d69c4
icedchai <70030002+icedchai@users.noreply.github.com>
2025-03-03 21:17:29 -0600

    Added things

---
+++ CraftSystem/CommonFuncs.cs
@@ -0,0 +1,26 @@
+﻿namespace CraftSystem
+{
+    using Exiled.API.Features;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Common functions used throughout the plugin.
+    /// </summary>
+    public static class CommonFuncs
+    {
+        /// <summary>
+        /// Checks if the given player is eligible to craft.
+        /// </summary>
+        /// <param name="player">The given player.</param>
+        /// <returns>Whether the player is eligible to craft.</returns>
+        public static bool CheckCraftEligible(Player player)
+        {
+            // TODO: ADD CHECK FOR CRAFTABILITY.
+            return true;
+        }
+    }
+}
---
+++ CraftSystem/EventHandler/CraftingEventHandlers.cs
@@ -0,0 +1,24 @@
+﻿namespace CraftSystem.EventHandlers
+{
+    using Exiled.Events.EventArgs.Player;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Contains all crafting-related event handlers.
+    /// </summary>
+    public class CraftingEventHandlers
+    {
+        /// <summary>
+        /// Handles submitting items to the crafting bench..
+        /// </summary>
+        /// <param name="e">The event args.</param>
+        public void OnDroppingItem(DroppingItemEventArgs e)
+        {
+
+        }
+    }
+}
