8e96cb080d5540b8238a660fa22e246da9fd662b
icedchai <70030002+icedchai@users.noreply.github.com>
2025-03-07 21:41:20 -0600

    Added CraftRecipe functions

--- CraftSystem/Customs/CraftRecipe.cs
+++ CraftSystem/Customs/CraftRecipe.cs
@@ -6,6 +6,7 @@
     using System.Text;
     using System.Threading.Tasks;
     using Exiled.API.Features;
+    using Exiled.API.Features.Items;
     using Exiled.CustomItems.API.Features;
 
     /// <summary>
@@ -14,8 +15,29 @@
     ///
     public class CraftRecipe
     {
+        /// <summary>
+        /// Contains the submitted items into a crafting table, accessible by their players.
+        /// </summary>
+        public static Dictionary<Player, HashSet<Item>> PlayerSubmittedItems = new Dictionary<Player, HashSet<Item>>();
+
+        /// <summary>
+        /// The dictionary of registered recipes, accessible by their name.
+        /// </summary>
+        private static Dictionary<string, CraftRecipe> registeredRecipes = new Dictionary<string, CraftRecipe>();
+
         private HashSet<string> recipeItems = new HashSet<string>();
         private HashSet<string> outputItems = new HashSet<string>();
+
+        /// <summary>
+        /// Gets a hash set of all <see cref="CraftRecipe"/>s currently registered.
+        /// </summary>
+        public static HashSet<CraftRecipe> RegisteredRecipes
+        {
+            get
+            {
+                return registeredRecipes.Values.ToHashSet();
+            }
+        }
 
         /// <summary>
         /// Gets or sets the name this recipe will be referenced under.
@@ -92,9 +114,14 @@
         public HashSet<CustomItem> GetRecipeCustomItems()
         {
             HashSet<CustomItem> items = new HashSet<CustomItem>();
+            uint id;
             foreach (string item in RecipeItems)
             {
-                if (CustomItem.TryGet(item, out CustomItem customItem))
+                if (!uint.TryParse(item, out id))
+                {
+                    break;
+                }
+                else if (CustomItem.TryGet(id, out CustomItem customItem))
                 {
                     items.Add(customItem);
                 }
@@ -145,18 +172,66 @@
         }
 
         /// <summary>
+        /// Gives a player the recipe output, and removes their crafting materials.
+        /// </summary>
+        /// <param name="player">The player to finalize the craft for.</param>
+        public static void FinalizeCraft(Player player)
+        {
+            if (!PlayerSubmittedItems.TryGetValue(player, out var submittedItems))
+            {
+                return;
+            }
+
+            HashSet<string> strings = CommonFuncs.HashItemsToHashString(submittedItems);
+            CraftRecipe recipe = CommonFuncs.GetCraftRecipe(strings);
+
+            // Null check to avoid NRE.
+            if (recipe is null)
+            {
+                return;
+            }
+
+            // Destroys all crafting materials.
+            foreach (Item item in PlayerSubmittedItems[player])
+            {
+                player.RemoveItem(item);
+            }
+
+            PlayerSubmittedItems.Clear();
+
+            if (player.Items.Count + recipe.OutputItems.Count > 8)
+            {
+                return;
+            }
+
+            // Give all custom items.
+            foreach (CustomItem citem in recipe.GetOutputCustomItems())
+            {
+                citem.Give(player);
+            }
+
+            // Give all vanilla items.
+            foreach (ItemType itemt in recipe.GetOutputItemTypes())
+            {
+                player.AddItem(itemt);
+            }
+
+            return;
+        }
+
+        /// <summary>
         /// Registers this recipe under <see cref="Plugin.RegisteredRecipes"/>.
         /// </summary>
         public void Register()
         {
-            if (Plugin.RegisteredRecipes.ContainsKey(RecipeName))
+            if (registeredRecipes.ContainsKey(RecipeName))
             {
                 RecipeName += "0";
                 Register();
                 return;
             }
 
-            Plugin.RegisteredRecipes.Add(RecipeName, this);
+            registeredRecipes.Add(RecipeName, this);
             Log.Info($"{RecipeName} registered!");
             return;
         }
